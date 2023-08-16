38c15876418d7d7067074d37281624bd6c2f6af0
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-16 17:54:09 +0200

    feat: add charging station status enum and case class

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
@@ -1,5 +1,25 @@
 package it.unibo.scs.chargingstation
 
-class ChargingStation {
+import scala.util.Random
 
-}
+enum ChargingStationStatus:
+  case FREE
+  case CHARGING
+  case RESERVED
+  case UNAVAILABLE //if the charging station has problems
+
+case class ChargingStation(id: Int, status: ChargingStationStatus, location: Point2D):
+  def changeState(newState: ChargingStationStatus): ChargingStation = ChargingStation(id, newState, location)
+  
+case class Point2D(var lat: Int = 0, var lon: Int = 0):
+  def createRandom(tx: Int, ty: Int, bx: Int, by: Int): Point2D =
+    val rand = new Random()
+    if tx <= ty then
+      lat = rand.between(tx, ty + 1)
+    else
+      lat = rand.between(ty, tx + 1)
+    if bx <= by then
+      lon = rand.between(bx, by + 1)
+    else
+      lon = rand.between(by, bx + 1)
+    Point2D(lat, lon)
