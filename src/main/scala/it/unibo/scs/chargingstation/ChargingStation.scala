3dc71f012ded0815a3251999794f6f306c7b1e84
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-17 21:36:38 +0200

    refactor: change status into state

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
@@ -2,14 +2,14 @@ package it.unibo.scs.chargingstation
 
 import scala.util.Random
 
-enum ChargingStationStatus:
+enum ChargingStationState:
   case FREE
   case CHARGING
   case RESERVED
   case UNAVAILABLE //if the charging station has problems
 
-case class ChargingStation(id: Int, status: ChargingStationStatus, location: Point2D):
-  def changeState(newState: ChargingStationStatus): ChargingStation = ChargingStation(id, newState, location)
+case class ChargingStation(id: Int, state: ChargingStationState, location: Point2D):
+  def changeState(newState: ChargingStationState): ChargingStation = ChargingStation(id, newState, location)
   
 case class Point2D(var lat: Int = 0, var lon: Int = 0):
   def createRandom(tx: Int, ty: Int, bx: Int, by: Int): Point2D =
