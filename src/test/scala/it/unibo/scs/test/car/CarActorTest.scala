fe45091b15a8911e9f48fba7f2c44028dcb2058b
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-17 15:21:59 +0200

    test: add car actor test

---
+++ src/test/scala/it/unibo/scs/test/car/CarActorTest.scala
@@ -0,0 +1,12 @@
+package it.unibo.scs.test.car
+
+import it.unibo.scs.car.{Car, CarActor}
+import it.unibo.scs.test.TestService
+
+class CarActorTest extends TestService:
+  val car = Car(0)
+  val sender = testKit.spawn(CarActor(car))
+  val probe = testKit.createTestProbe[CarActor.CarUpdated]()
+  sender ! CarActor.StartCharge()
+  sender ! CarActor.SendCharge(0.1, probe.ref)
+  probe.expectMessage(CarActor.CarUpdated(car.copy(charge = 0.1)))
