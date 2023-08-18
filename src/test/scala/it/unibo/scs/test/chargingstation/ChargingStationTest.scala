58a1940131220e6b8c1b7d75c811af29fa06875e
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-18 13:49:16 +0200

    test: update test style and refactor test

--- src/test/scala/it/unibo/scs/test/chargingstation/ChargingStationTest.scala
+++ src/test/scala/it/unibo/scs/test/chargingstation/ChargingStationTest.scala
@@ -1,15 +1,17 @@
 package it.unibo.scs.test.chargingstation
 
-import it.unibo.scs.chargingstation.{ChargingStation, ChargingStationActor, ChargingStationState, Point2D}
+import it.unibo.scs.chargingstation.{ChargingStation, ChargingStationActor, ChargingStationEvents, ChargingStationState, Point2D}
 import it.unibo.scs.test.TestService
 
 class ChargingStationTest extends TestService:
-  val chargingStation = ChargingStation(id = 0, state = ChargingStationState.FREE, location = Point2D())
-  val sender = testKit.spawn(ChargingStationActor(chargingStation))
-  val probe = testKit.createTestProbe[ChargingStationActor.UpdateChargingStation]()
-  sender ! ChargingStationActor.SendState(chargingStation.id, probe.ref)
-  probe.expectMessage(ChargingStationActor.UpdateChargingStation(chargingStation))
+  "A charging station" should {
+    "be created" in {
+      val chargingStation = ChargingStation(id = 0, state = ChargingStationState.FREE, location = Point2D())
+      val sender = testKit.spawn(ChargingStationActor(chargingStation))
+      val probe = testKit.createTestProbe[ChargingStationEvents.ChargingStationListener]()
+      sender ! ChargingStationEvents.AskState(probe.ref)
+      probe.expectMessage(ChargingStationEvents.ChargingStationUpdated(chargingStation))
+    }
+  }
 
-  val newState = ChargingStationState.CHARGING
-  sender ! ChargingStationActor.ChangeState(csID = chargingStation.id, newState = newState, probe.ref)
-  probe.expectMessage(ChargingStationActor.UpdateChargingStation(chargingStation.copy(state = newState)))
+
