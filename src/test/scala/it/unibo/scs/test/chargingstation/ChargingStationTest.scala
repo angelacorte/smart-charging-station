867e42b8b3711a2debf29d26ee6656031cc573e0
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-19 11:45:35 +0200

    test: add test for charging response and refactoring

--- src/test/scala/it/unibo/scs/test/chargingstation/ChargingStationTest.scala
+++ src/test/scala/it/unibo/scs/test/chargingstation/ChargingStationTest.scala
@@ -2,16 +2,27 @@ package it.unibo.scs.test.chargingstation
 
 import it.unibo.scs.chargingstation.{ChargingStation, ChargingStationActor, ChargingStationEvents, ChargingStationState, Point2D}
 import it.unibo.scs.test.TestService
+import it.unibo.scs.car.{Car, CarActor}
+
 
 class ChargingStationTest extends TestService:
   "A charging station" should {
     "be created" in {
       val chargingStation = ChargingStation(id = 0, state = ChargingStationState.FREE, location = Point2D())
       val sender = testKit.spawn(ChargingStationActor(chargingStation))
-      val probe = testKit.createTestProbe[ChargingStationEvents.ChargingStationListener]()
+      val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
       sender ! ChargingStationEvents.AskState(probe.ref)
       probe.expectMessage(ChargingStationEvents.ChargingStationUpdated(chargingStation))
     }
+
+    "not accept charging request if not free" in {
+      val chargingStation = ChargingStation(id = 0, state = ChargingStationState.CHARGING, location = Point2D())
+      val sender = testKit.spawn(ChargingStationActor(chargingStation))
+      val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
+      sender ! ChargingStationEvents.Charge(probe.ref)
+      probe.expectMessage(ChargingStationEvents.NotOk(chargingStation.state))
+    }
   }
+
 
 
