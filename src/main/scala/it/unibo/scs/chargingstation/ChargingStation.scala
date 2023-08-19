c92cf6a764cd6dea6ae1f133398309dc72da70ff
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-19 12:21:14 +0200

    refactor: removed status and position from charging station

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
@@ -8,18 +8,4 @@ enum ChargingStationState:
   case RESERVED
   case UNAVAILABLE //if the charging station has problems
 
-case class ChargingStation(id: Int, state: ChargingStationState, location: Point2D):
-  def changeState(newState: ChargingStationState): ChargingStation = ChargingStation(id, newState, location)
-  
-case class Point2D(var lat: Int = 0, var lon: Int = 0):
-  def createRandom(tx: Int, ty: Int, bx: Int, by: Int): Point2D =
-    val rand = new Random()
-    if tx <= ty then
-      lat = rand.between(tx, ty + 1)
-    else
-      lat = rand.between(ty, tx + 1)
-    if bx <= by then
-      lon = rand.between(bx, by + 1)
-    else
-      lon = rand.between(by, bx + 1)
-    Point2D(lat, lon)
+case class ChargingStation(id: Int)
