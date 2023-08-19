9a309355bac199fe9dd5746432c32ae3b2367868
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-19 12:23:38 +0200

    test: complete charging station tests

--- src/test/scala/it/unibo/scs/test/chargingstation/ChargingStationTest.scala
+++ src/test/scala/it/unibo/scs/test/chargingstation/ChargingStationTest.scala
@@ -1,34 +1,48 @@
 package it.unibo.scs.test.chargingstation
 
-import it.unibo.scs.chargingstation.{ChargingStation, ChargingStationActor, ChargingStationEvents, ChargingStationState, Point2D}
+import it.unibo.scs.chargingstation.{ChargingStation, ChargingStationActor, ChargingStationEvents, ChargingStationState}
 import it.unibo.scs.test.TestService
 import it.unibo.scs.car.{Car, CarActor}
+import it.unibo.scs.chargingstation.ChargingStationEvents.SendChargeFromChargingStation
 
 
 class ChargingStationTest extends TestService:
   "A charging station" should {
+
+    val chargingStation = ChargingStation(id = 0)
+    val sender = testKit.spawn(ChargingStationActor(chargingStation))
+    val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
+
     "be created" in {
-      val chargingStation = ChargingStation(id = 0, state = ChargingStationState.FREE, location = Point2D())
-      val sender = testKit.spawn(ChargingStationActor(chargingStation))
-      val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
       sender ! ChargingStationEvents.AskState(probe.ref)
       probe.expectMessage(ChargingStationEvents.ChargingStationUpdated(chargingStation))
     }
 
     "accept charging request if free" in {
-      val chargingStation = ChargingStation(id = 0, state = ChargingStationState.FREE, location = Point2D())
-      val sender = testKit.spawn(ChargingStationActor(chargingStation))
-      val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
       sender ! ChargingStationEvents.Charge(probe.ref)
       probe.expectMessage(ChargingStationEvents.Ok())
+      probe.expectMessage(SendChargeFromChargingStation(5.0))
     }
 
     "not accept charging request if not free" in {
-      val chargingStation = ChargingStation(id = 0, state = ChargingStationState.CHARGING, location = Point2D())
-      val sender = testKit.spawn(ChargingStationActor(chargingStation))
-      val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
+      //inviare qualcosa di diverso da charge cosi cambia lo stato
       sender ! ChargingStationEvents.Charge(probe.ref)
-      probe.expectMessage(ChargingStationEvents.NotOk(chargingStation.state))
+      probe.expectMessage(ChargingStationEvents.NotOk(ChargingStationState.CHARGING))
+    }
+
+    "stop the charge" in {
+      sender ! ChargingStationEvents.StopCharge()
+      probe.expectNoMessage()
+    }
+
+    "accept reserve request if free" in {
+      sender ! ChargingStationEvents.Reserve(probe.ref)
+      probe.expectMessage(ChargingStationEvents.Ok())
+    }
+
+    "not accept reserve request if not free" in {
+      sender ! ChargingStationEvents.Charge(probe.ref)
+      probe.expectMessage(ChargingStationEvents.NotOk(ChargingStationState.RESERVED))
     }
   }
 
