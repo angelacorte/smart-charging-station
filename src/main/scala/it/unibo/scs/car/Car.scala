7b5010f5c5fab0cf7ada6a543ff01fe0f87c2be0
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-21 10:13:41 +0200

    refactor: refactor car

--- src/main/scala/it/unibo/scs/car/Car.scala
+++ src/main/scala/it/unibo/scs/car/Car.scala
@@ -1,3 +1,12 @@
 package it.unibo.scs.car
 
-case class Car(charge: Double)
+trait Car:
+  val charge: Double
+  val maxCharge = 1
+  
+object Car:
+  def apply(): Car = CarImpl(1)
+  def apply(charge: Double): Car = CarImpl(charge)
+  private class CarImpl(val charge: Double) extends Car
+  
+  
