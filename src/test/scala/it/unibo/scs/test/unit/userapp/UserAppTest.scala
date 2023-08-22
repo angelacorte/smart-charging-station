a2f4cbacc2abe1ef39bcc6426f6b71be154d542e
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-08-22 12:53:25 +0200

    refactor(test): refactor class name

--- src/test/scala/it/unibo/scs/test/unit/userapp/UserAppTest.scala
+++ src/test/scala/it/unibo/scs/test/unit/userapp/UserAppTest.scala
@@ -1,12 +1,17 @@
 package it.unibo.scs.test.unit.userapp
 
+import akka.actor.testkit.typed.javadsl.LoggingTestKit
+import it.unibo.scs.car.{Battery, Car, ControlUnit}
+import it.unibo.scs.chargingstation.{ChargingStation, ChargingStationActor}
 import it.unibo.scs.test.TestService
+import it.unibo.scs.userapp.UserAppEvents.Tick
 import it.unibo.scs.userapp.{UserApp, UserAppActor, UserAppEvents}
 
-class ChargingStationTest extends TestService:
+class UserAppTest extends TestService:
   "A user app" should {
-    val userapp = UserApp(0)
-    val sender = testKit.spawn(UserAppActor(userapp))
+    val controlUnitActors = Set(testKit.spawn(ControlUnit(Car(Battery()))))
+    val chargingStationsActors = (1 to 5).map(id => testKit.spawn(ChargingStationActor(ChargingStation(id)))).toSet
+    val sender = testKit.spawn(UserAppActor(controlUnitActors, chargingStationsActors))
     val probe = testKit.createTestProbe[UserAppEvents.Response]()
 
     "do something" in {
