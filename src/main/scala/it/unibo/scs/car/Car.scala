9a2fe46a339ee25441372177a525520c2a4d4c81
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-23 16:29:23 +0200

    feat: add battery status

--- src/main/scala/it/unibo/scs/car/Car.scala
+++ src/main/scala/it/unibo/scs/car/Car.scala
@@ -2,6 +2,10 @@ package it.unibo.scs.car
 
 case class Car(battery: Battery)
 
+enum BatteryStatus:
+  case On
+  case Off
+  
 case class Battery(currentCharge: Double = 1):
   def charge(amount: Double): Battery = copy(currentCharge = currentCharge + amount)
   def discharge: Battery = copy(currentCharge = currentCharge - 0.1)
