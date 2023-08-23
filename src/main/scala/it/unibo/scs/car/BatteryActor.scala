1237b69c09da2fff375d3a7a6737d7218676c609
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-23 16:30:13 +0200

    feat: add event for battery actor

--- src/main/scala/it/unibo/scs/car/BatteryActor.scala
+++ src/main/scala/it/unibo/scs/car/BatteryActor.scala
@@ -2,6 +2,8 @@ package it.unibo.scs.car
 
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import it.unibo.scs.car.BatteryStatus.{On, Off}
+
 import concurrent.duration.DurationInt
 
 object BatteryActor:
@@ -10,6 +12,8 @@ object BatteryActor:
   case class TurnOff() extends Command
   private case class Charge(amount: Double) extends Command
   private case class Discharge() extends Command
+  final case class DebugBattery(replyTo: ActorRef[BatteryUpdated]) extends Command
+  final case class BatteryUpdated(battery: Battery, status: BatteryStatus)
   
 
   def apply(battery: Battery, cu: ActorRef[ControlUnit.Request]): Behavior[BatteryActor.Command] =
@@ -22,11 +26,14 @@ object BatteryActor:
       Behaviors receiveMessage {
         case Discharge() =>
           println(s"BatteryActor: Discharge, current charge: ${battery.currentCharge}")
-          cu ! ControlUnit.BatteryUpdated(battery.discharge)
+          cu ! ControlUnit.BatteryUpdated(battery.discharge, On)
           on(battery.discharge, cu)
         case TurnOff() =>
           println("BatteryActor: TurnOff")
           off(battery, cu)
+        case DebugBattery(replyTo) =>
+          replyTo ! BatteryUpdated(battery, On)
+          on(battery, cu)
         case _ =>
           on(battery, cu)
       }
@@ -39,8 +46,11 @@ object BatteryActor:
         on(battery, cu)
       case Charge(amount) =>
         println(s"BatteryActor: Charge, current charge: ${battery.currentCharge}")
-        cu ! ControlUnit.BatteryUpdated(battery.charge(amount))
+        cu ! ControlUnit.BatteryUpdated(battery.charge(amount), Off)
         off(battery.charge(amount), cu)
+      case DebugBattery(replyTo) =>
+        replyTo ! BatteryUpdated(battery, Off)
+        off(battery, cu)
       case _ =>
         off(battery, cu)
     }
