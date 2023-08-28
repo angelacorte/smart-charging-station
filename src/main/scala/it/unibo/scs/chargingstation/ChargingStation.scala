c7e50eea03d954069618c645ab1b9fc1eb98f8ee
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-28 15:20:03 +0200

    feat: add givens for marshalling and unmarshalling

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStation.scala
@@ -3,8 +3,27 @@ package it.unibo.scs.chargingstation
 import it.unibo.scs.CborSerializable
 import it.unibo.scs.chargingstation.ChargingStationState.ChargingStationState
 
-object ChargingStationState extends Enumeration:
-  type ChargingStationState = Value
-  val FREE, CHARGING, RESERVED, UNAVAILABLE = Value
+object ChargingStation:
+  object ChargingStationState extends Enumeration with Serializable:
+    type ChargingStationState = Value
+    val FREE, CHARGING, RESERVED, UNAVAILABLE = Value
 
-case class ChargingStation(id: Int, state: ChargingStationState = ChargingStationState.FREE) extends CborSerializable
+  import ChargingStationState._
+  
+  case class ChargingStation(id: Int, state: ChargingStationState = ChargingStationState.FREE) extends CborSerializable
+
+  object Formats:
+    given stateFormatter: RootJsonFormat[ChargingStationState] with
+      override def write(obj: ChargingStationState.Value): JsValue = JsString(obj.toString)
+      override def read(json: JsValue): ChargingStationState = json match
+        case JsString(value) => ChargingStationState.withName(value)
+        case _ => throw DeserializationException("Enum string expected")
+        
+    given stationFormat: RootJsonFormat[ChargingStation] = jsonFormat2(ChargingStation.apply)
+
+    given listFormat: RootJsonFormat[List[ChargingStation]] with
+      override def write(obj: List[ChargingStation]): JsValue = JsArray(obj.map(_.toJson).toVector)
+      override def read(json: JsValue): List[ChargingStation] = json match
+        case JsArray(elements) => elements.map(_.convertTo[ChargingStation]).toList
+        case _ => throw DeserializationException("Enum string expected")
+    
