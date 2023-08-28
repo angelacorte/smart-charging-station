7149641238341235ae62a06c05af6c584102bf43
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-28 15:40:28 +0200

    chore: optimize imports

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStationService.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStationService.scala
@@ -4,14 +4,9 @@ import akka.actor.typed.scaladsl.AskPattern.*
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.*
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
 import akka.http.scaladsl.server.Directives.*
-
 import akka.util.Timeout
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
-
 import it.unibo.scs.chargingstation.ChargingStation.*
 import it.unibo.scs.chargingstation.ChargingStationProvider.GetChargingStations
 
