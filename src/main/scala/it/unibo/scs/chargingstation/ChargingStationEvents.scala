b082a03fce34ff9041013402848f1af7106fe4b4
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-28 10:55:16 +0200

    fix: make events serializable with cbor

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStationEvents.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStationEvents.scala
@@ -1,12 +1,13 @@
 package it.unibo.scs.chargingstation
 
 import akka.actor.typed.ActorRef
+import it.unibo.scs.CborSerializable
 import it.unibo.scs.car.ControlUnit
 import it.unibo.scs.chargingstation.ChargingStationState.ChargingStationState
 
 object ChargingStationEvents:
   sealed trait Request
-  case class AskState(replyTo: ActorRef[Response]) extends Request
+  case class AskState(replyTo: ActorRef[Response]) extends Request with CborSerializable
   case class Charge(replyTo: ActorRef[Response]) extends Request
   case class Reserve(replyTo: ActorRef[Response]) extends Request
   case class Tick() extends Request
@@ -17,5 +18,5 @@ object ChargingStationEvents:
   case class SendChargeFromChargingStation(charge: Double, replyTo: ActorRef[ChargingStationEvents.Request]) extends Response
   case class Ok() extends Response
   case class NotOk(state: ChargingStationState) extends Response
-  case class ChargingStationUpdated(chargingStation: ChargingStation, ref: ActorRef[ChargingStationEvents.Request]) extends Response
+  case class ChargingStationUpdated(chargingStation: ChargingStation, ref: ActorRef[ChargingStationEvents.Request]) extends Response with CborSerializable
 
