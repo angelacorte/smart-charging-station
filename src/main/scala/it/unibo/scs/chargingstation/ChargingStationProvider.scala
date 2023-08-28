d28a8cef54a818137dfc87037127d209a958fa8d
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-28 10:55:16 +0200

    fix: make events serializable with cbor

--- src/main/scala/it/unibo/scs/chargingstation/ChargingStationProvider.scala
+++ src/main/scala/it/unibo/scs/chargingstation/ChargingStationProvider.scala
@@ -3,13 +3,14 @@ package it.unibo.scs.chargingstation
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
 import akka.actor.typed.scaladsl.Behaviors
+import it.unibo.scs.CborSerializable
 import it.unibo.scs.chargingstation.ChargingStationActor.ChargingStationServiceKey
 
 object ChargingStationProvider:
   private type ChargingStationRegistry = Map[ActorRef[ChargingStationEvents.Request], ChargingStation]
   sealed trait Request
-  case class GetChargingStations(replyTo: ActorRef[Set[ChargingStation]]) extends Request
-  case class UpdateChargingStation(chargingStation: ChargingStation, ref: ActorRef[ChargingStationEvents.Request]) extends Request
+  case class GetChargingStations(replyTo: ActorRef[Set[ChargingStation]]) extends Request with CborSerializable
+  case class UpdateChargingStation(chargingStation: ChargingStation, ref: ActorRef[ChargingStationEvents.Request]) extends Request with CborSerializable
   private case class ChargingStationsUpdated(chargingStations: Set[ActorRef[ChargingStationEvents.Request]]) extends Request
   private case class BadRequest() extends Request
 
