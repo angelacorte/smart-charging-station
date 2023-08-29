65f7acff65005ffe5f65372c4c1c1261c6fc6f55
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-29 22:55:15 +0200

    feat: add a message type for reservation result

--- src/main/scala/it/unibo/scs/cluster/chargingstation/ChargingStationActor.scala
+++ src/main/scala/it/unibo/scs/cluster/chargingstation/ChargingStationActor.scala
@@ -25,53 +25,51 @@ object ChargingStationActor:
       free(chargingStation, providers)
     }
 
-  private def free(chargingStation: ChargingStation, providers: Set[ActorRef[ChargingStationProvider.Request]]) : Behavior[ChargingStationEvents.Request] =
+  private def free(chargingStation: ChargingStation, providers: Set[ActorRef[ChargingStationProvider.Request]]): Behavior[ChargingStationEvents.Request] =
     Behaviors receive {
       case (ctx, AskState(replyTo)) =>
         replyTo ! ChargingStationUpdated(chargingStation, ctx.self)
         free(chargingStation, providers)
       case (_, Charge(replyTo)) =>
           replyTo ! Ok()
-          charging(chargingStation.copy(state = ChargingStationState.CHARGING), replyTo, providers)
+          charging(chargingStation.copy(state = ChargingStationState.CHARGING), providers)
       case (_, Reserve(replyTo)) =>
-          replyTo ! Ok()
-          reserved(chargingStation.copy(state = ChargingStationState.RESERVED), replyTo, providers)
+          replyTo ! ReservationOk()
+          reserved(chargingStation.copy(state = ChargingStationState.RESERVED), providers)
       case _ => free(chargingStation, providers)
     }
 
-  private def charging(chargingStation: ChargingStation, replyTo: ActorRef[Response], providers: Set[ActorRef[ChargingStationProvider.Request]]): Behavior[ChargingStationEvents.Request] =
+  private def charging(chargingStation: ChargingStation, providers: Set[ActorRef[ChargingStationProvider.Request]]): Behavior[ChargingStationEvents.Request] =
     Behaviors withTimers { timers =>
       timers.startTimerWithFixedDelay(Tick(), 2.seconds)
       Behaviors receive {
         case (ctx, AskState(replyTo)) =>
           replyTo ! ChargingStationUpdated(chargingStation, ctx.self)
-          charging(chargingStation, replyTo, providers)
+          charging(chargingStation, providers)
         case (_, Charge(replyTo)) =>
           replyTo ! NotOk(ChargingStationState.CHARGING)
-          charging(chargingStation, replyTo, providers)
+          charging(chargingStation, providers)
         case (_, Reserve(replyTo)) =>
-          replyTo ! NotOk(ChargingStationState.CHARGING)
-          charging(chargingStation, replyTo, providers)
+          replyTo ! ReservationNotOk("This charging station is busy")
+          charging(chargingStation, providers)
         case (_, StopCharge()) =>
           free(chargingStation.copy(state = ChargingStationState.FREE), providers)
-        case (ctx, Tick()) =>
-          replyTo ! SendChargeFromChargingStation(5.0, ctx.self)
-          charging(chargingStation, replyTo, providers)
-        case _ => charging(chargingStation, replyTo, providers)
+        case (_, Tick()) =>
+          charging(chargingStation, providers)
+        case _ => charging(chargingStation, providers)
       }
     }
 
-  private def reserved(chargingStation: ChargingStation, replyTo: ActorRef[Response], providers: Set[ActorRef[ChargingStationProvider.Request]]): Behavior[ChargingStationEvents.Request] =
+  private def reserved(chargingStation: ChargingStation, providers: Set[ActorRef[ChargingStationProvider.Request]]): Behavior[ChargingStationEvents.Request] =
     Behaviors receive {
       case (ctx, AskState(replyTo)) =>
         replyTo ! ChargingStationUpdated(chargingStation, ctx.self)
-        reserved(chargingStation, replyTo, providers)
+        reserved(chargingStation, providers)
       case (_, Charge(replyTo)) =>
-        /**
-         * Contattare un db o altro per capire chi ha prenotato la colonnina
-         * la risposta va di conseguenza
-         */
-        replyTo ! NotOk(ChargingStationState.RESERVED)
-        charging(chargingStation.copy(state = ChargingStationState.CHARGING), replyTo, providers)
-      case _ => reserved(chargingStation, replyTo, providers)
+        // TODO implement
+        charging(chargingStation.copy(state = ChargingStationState.CHARGING), providers)
+      case (_, Reserve(replyTo)) =>
+        replyTo ! ReservationNotOk("This charging station is already reserved")
+        reserved(chargingStation, providers)
+      case _ => reserved(chargingStation, providers)
     }
