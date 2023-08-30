b33a2f4cf93dba88b4f6017c16a9e0237f8c3486
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-08-30 12:04:05 +0200

    fix(test): fixed and refactored unit tests

--- src/test/scala/it/unibo/scs/test/unit/chargingstation/ChargingStationTest.scala
+++ src/test/scala/it/unibo/scs/test/unit/chargingstation/ChargingStationTest.scala
@@ -1,50 +1,52 @@
 package it.unibo.scs.test.unit.chargingstation
 
 import it.unibo.scs.cluster.chargingstation.{ChargingStationActor, ChargingStationEvents}
-import it.unibo.scs.test.TestService
-import it.unibo.scs.cluster.chargingstation.ChargingStationEvents.SendChargeFromChargingStation
+import it.unibo.scs.model.chargerequest.ChargeRequest
 import it.unibo.scs.model.chargingstation.{ChargingStation, ChargingStationState, Position}
-import it.unibo.scs.model.chargingstation.ChargingStationState.*
+import it.unibo.scs.model.reservation.Reservation
+import it.unibo.scs.test.TestService
 
 
 class ChargingStationTest extends TestService:
   "A charging station" should {
 
     val chargingStation = ChargingStation(0, "Enel", "X Charging Station", 100, Position(44.17457186518429, 12.23658150624628))
+    val chargeRequest = ChargeRequest(0, 0)
+    val reservation = Reservation(0, 0)
 
     val sender = testKit.spawn(ChargingStationActor(chargingStation))
-    val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
+
 
     "be created" in {
+      val probe = testKit.createTestProbe[ChargingStationEvents.Response]()
       sender ! ChargingStationEvents.AskState(probe.ref)
       probe.expectMessage(ChargingStationEvents.ChargingStationUpdated(chargingStation, sender.ref))
     }
 
     "accept charging request if free" in {
-      sender ! ChargingStationEvents.Charge(probe.ref)
-      probe.expectMessage(ChargingStationEvents.Ok())
-      probe.expectMessage(SendChargeFromChargingStation(5.0, sender.ref))
+      val probe = testKit.createTestProbe[ChargingStationEvents.ChargeRequestResult]()
+      sender ! ChargingStationEvents.Charge(chargeRequest, probe.ref)
+      probe.expectMessage(ChargingStationEvents.ChargeRequestOk())
     }
 
     "not accept charging request if not free" in {
-      //inviare qualcosa di diverso da charge cosi cambia lo stato
-      sender ! ChargingStationEvents.Charge(probe.ref)
-      probe.expectMessage(ChargingStationEvents.NotOk(ChargingStationState.CHARGING))
+      val probe = testKit.createTestProbe[ChargingStationEvents.ChargeRequestResult]()
+      sender ! ChargingStationEvents.Charge(chargeRequest, probe.ref)
+      probe.expectMessage(ChargingStationEvents.ChargeRequestNotOk("This charging station is busy"))
     }
 
-    "stop the charge" in {
-      sender ! ChargingStationEvents.StopCharge()
-      probe.expectNoMessage()
-    }
 
     "accept reserve request if free" in {
-      sender ! ChargingStationEvents.Reserve(probe.ref)
-      probe.expectMessage(ChargingStationEvents.Ok())
+      val probe = testKit.createTestProbe[ChargingStationEvents.ReservationResult]()
+      sender ! ChargingStationEvents.StopCharge()
+      sender ! ChargingStationEvents.Reserve(reservation, probe.ref)
+      probe.expectMessage(ChargingStationEvents.ReservationOk())
     }
 
     "not accept reserve request if not free" in {
-      sender ! ChargingStationEvents.Charge(probe.ref)
-      probe.expectMessage(ChargingStationEvents.NotOk(ChargingStationState.RESERVED))
+      val probe = testKit.createTestProbe[ChargingStationEvents.ReservationResult]()
+      sender ! ChargingStationEvents.Reserve(Reservation(1,0), probe.ref)
+      probe.expectMessage(ChargingStationEvents.ReservationNotOk("This charging station is already reserved"))
     }
   }
 
