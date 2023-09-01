ae138d74923e2b69e7906454d97ebfbcc9bf1519
LEONARDO MICELLI <leonardomicelli@gmail.com>
2023-09-01 12:30:05 +0200

    docs: add comments

--- src/main/scala/it/unibo/scs/model/reservation/Reservation.scala
+++ src/main/scala/it/unibo/scs/model/reservation/Reservation.scala
@@ -2,4 +2,9 @@ package it.unibo.scs.model.reservation
 
 import it.unibo.scs.CborSerializable
 
+/**
+ * A reservation is a request to reserve a charging station for a user.
+ * @param userId The user that wants to reserve a charging station.
+ * @param chargingStationId The charging station that the user wants to reserve.
+ */
 case class Reservation(userId: String, chargingStationId: Int) extends CborSerializable
